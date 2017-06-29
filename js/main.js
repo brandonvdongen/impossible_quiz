6f3d709e4f254494045741763088b11cdbd52344
student <brandonvdongen.blk>
2017-06-29 09:59:27 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    question =
+   document.addEventListener("click",function(e){
+       console.log(e.target.parentNode.id);
+   });
+});
